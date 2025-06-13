d7142e6215d119316185e6c744bcb713362e8360
Hoang Anh <ngphuchoanganh@gmail.com>
2025-06-13 22:12:22 +0700

    refactor: remove unused code

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,39 @@
 import { envConfig } from "@config"
-import { app } from "./server.ts"
 
-async function handleShutdown(signal: string) {
+import { etag } from "@bogeychan/elysia-etag"
+import { cors } from "@elysiajs/cors"
+import { html } from "@elysiajs/html"
+import { swagger } from "@elysiajs/swagger"
+import { customPlugin } from "@plugin"
+import { logger } from "@tqman/nice-logger"
+import { Elysia } from "elysia"
+import { autoload } from "elysia-autoload"
+import { helmet } from "elysia-helmet"
+import type { SignalsListener } from "bun"
+
+export const app = new Elysia()
+	.use(logger())
+	.use(swagger())
+	.use(cors())
+	.use(html())
+	.use(etag())
+	.use(helmet())
+	.use(customPlugin())
+	.use(await autoload())
+	.onStart(({ server }) => {
+		console.log(`🦊 Server started at ${server?.url.origin}`)
+	})
+	.listen(envConfig.PORT)
+
+export type ElysiaApp = typeof app
+
+const handleShutdown: SignalsListener = async (signal) => {
 	console.log(`Received ${signal}. Initiating graceful shutdown...`)
 	await app.stop()
 	process.exit(0)
 }
 
 process
-	.on("SIGINT", (signal) => handleShutdown(signal))
-	.on("SIGTERM", (signal) => handleShutdown(signal))
-	.on("uncaughtException", (error) => {
-		console.error(error)
-	})
-	.on("unhandledRejection", (error) => {
-		console.error(error)
-	})
-
-app.listen(envConfig.PORT, ({ url }) =>
-	console.log(`🦊 Server started at ${url.origin}`)
-)
+	.on("SIGINT", handleShutdown)
+	.on("SIGTERM", handleShutdown)
+	.on("uncaughtException", handleShutdown)
